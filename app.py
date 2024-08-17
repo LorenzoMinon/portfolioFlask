72b25c9a5deb74f680982628ff03d25f6ddefd89
Lorenzo Miñón <lorenzominon01@gmail.com>
2024-08-17 20:27:14 -0300

    Base app running

--- app.py
+++ app.py
@@ -2,25 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/') # Home page
-
+@app.route('/')
 def inicio():
     return render_template('index.html')
 
-@app.route('/about') # About page
-
+@app.route('/sobre-mi')
 def sobre_mi():
-    return render_template('about.html')
+    return render_template('sobre_mi.html')
+
+@app.route('/tecnologias')
+def tecnologias():
+    return render_template('tecnologias.html')
 
 @app.route('/proyectos')
-
 def proyectos():
     return render_template('proyectos.html')
 
-@app.route('proyectos/<int:proyecto_id>')
-
+@app.route('/proyecto/<int:proyecto_id>')
 def proyecto_detalle(proyecto_id):
-    return render_template('proyecto_detalle.html', proyecto_id=proyecto_id)    
+    # Aquí puedes cargar los detalles de un proyecto según su ID
+    return render_template('proyecto_detalle.html', proyecto_id=proyecto_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
